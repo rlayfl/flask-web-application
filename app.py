fc1f66cae29a690a50c83b82512a56992a3d8626
rlayfl <rlayfl@essex.ac.uk>
2024-03-23 17:08:09 +0000

    Working input data into database from form

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from datetime import datetime
+
 import json
 import urllib.parse
 
@@ -69,7 +71,22 @@ def add_new_module():
     json_object = json.loads(json_string)
 
     # Print JSON object
-    print(json_object)
+    print(json_object["code"])
+    print(json_object["name"])
+    print(json_object["description"])
+    print(json_object["startDate"])
+    print(json_object["endDate"])
+
+    newModule = Modules(
+        name=json_object["name"],
+        code=json_object["code"],
+        description=json_object["description"],
+        startDate=datetime.strptime(json_object["startDate"], '%Y-%m-%d'),
+        endDate=datetime.strptime(json_object["endDate"], '%Y-%m-%d')
+    )
+
+    db.session.add(newModule)
+    db.session.commit()
 
     return "Hello"
 
